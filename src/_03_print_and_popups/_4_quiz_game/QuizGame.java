3117fde885783119205712311f187d164aee9ebc
league <league@imac-14.attlocal.net>
2022-05-29 11:24:02 -0700

    yes

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,29 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String question = JOptionPane.showInputDialog ("What is 69x69");
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if (question.equalsIgnoreCase("4761")) {
+				score += 1;
+				}
 				// 4.  if the user's answer was correct, add one to their score 
-		
+				String questin = JOptionPane.showInputDialog ("What is the name for the thing at the end of a shoelace?");
+				if (question.equalsIgnoreCase("aglet")) {
+					score += 1;
+					}
+				String questi = JOptionPane.showInputDialog ("Whats the capital of North Macedonia?");
+				if (question.equalsIgnoreCase("Skopje")) {
+					score += 1;
+					}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
